93a2c3b08e6e5b9720576c4293973d1674c23caa
NagendraMekala <nagendra.kldm@gmail.com>
2018-12-02 09:07:11 -0800

    updated files

--- maven-jenkins/src/main/java/com/mng/java/App.java
+++ maven-jenkins/src/main/java/com/mng/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World well come to jenkins" );
+        System.out.println( " SHello World well come to jenkins" );
     }
 }
